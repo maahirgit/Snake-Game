0fa5cd40543b41ac5dbbbe5c7dbeb6631369cc8b
Maahirgit <Maahirgit@gmail.com>
2025-11-02 20:43:32 +0530

    working of snake including movements

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -5,12 +5,88 @@
 #include <vector>
 using namespace std;
 
+
+class Snake
+{
+public:
+
+    deque<pair<int, int>> body; 
+
+    Direction dir;
 Snake(int startX = 1, int startY = 1)
     {
         dir = STOP;
         body.clear();
-        // PDF Requirement: Snake starts as a line with 3 cells
-        body.push_back({startX, startY}); // head
+        body.push_back({startX, startY}); 
         body.push_back({startX, startY + 1});
-        body.push_back({startX, startY + 2}); // tail
+        body.push_back({startX, startY + 2}); 
     }
+
+
+    void reset(int startX, int startY)
+    {
+        dir = STOP;
+        body.clear();
+        body.push_back({startX, startY});
+        body.push_back({startX, startY + 1});
+        body.push_back({startX, startY + 2});
+    }
+
+    void changeDirection(Direction newDir)
+    {
+        if ((dir == LEFT && newDir == RIGHT) || (dir == RIGHT && newDir == LEFT) ||
+            (dir == UP && newDir == DOWN) || (dir == DOWN && newDir == UP))
+            return;
+        dir = newDir;
+    }
+void move()
+    {
+        if (dir == STOP)
+            return;
+
+        pair<int, int> newHead = body.front();
+        switch (dir)
+        {
+        case LEFT:
+            newHead.first--;
+            break;
+        case RIGHT:
+            newHead.first++;
+            break;
+        case UP:
+            newHead.second--;
+            break;
+        case DOWN:
+            newHead.second++;
+            break;
+        default:
+            break;
+        }
+        body.push_front(newHead);
+        body.pop_back();
+    }
+
+    void grow()
+    {
+        body.push_back(body.back());
+    }
+
+    bool occupies(int x, int y) const
+    {
+        for (const auto &p : body)
+            if (p.first == x && p.second == y)
+                return true;
+        return false;
+    }
+
+    bool checkSelfCollision() const
+    {
+        if (body.size() < 4)
+            return false;
+        auto head = body.front();
+        for (size_t i = 1; i < body.size(); ++i)
+            if (body[i].first == head.first && body[i].second == head.second)
+                return true;
+        return false;
+    }
+};
